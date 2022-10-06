e6b95b4e4c6718ea829455be25b3cab6c3baee69
Gabriel <gabrielja2antunes@gmail.com>
2022-10-06 20:10:29 -0300

    feat: criando UserController

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import UserService from '../services/UserService';
+
+export default class ProductController {
+  private service: UserService;
+
+  constructor() {
+    this.service = new UserService();
+  }
+
+  public newUser = async (req: Request, res: Response) => {
+    const token = await this.service.createNewUser(req.body);
+
+    res.status(201).json(token);
+  };  
+}
