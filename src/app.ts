cae182b87395cf905f45ccc6b4218340c71d011f
Gabriel <gabrielja2antunes@gmail.com>
2022-10-06 18:28:14 -0300

    chore: implementando rota no app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
+import httpErrorMiddleware from './middlewares/http.error.middleware';
+import productRouter from './routers/productRouter';
 
 const app = express();
-
 app.use(express.json());
+
+app.use('/products', productRouter);
+app.use(httpErrorMiddleware);
 
 export default app;
