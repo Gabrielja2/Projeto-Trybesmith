869379e42f0b53e26fd4e8361491d27746f3263f
Gabriel <gabrielja2antunes@gmail.com>
2022-10-06 20:10:49 -0300

    feat: criando UserModel

---
+++ src/models/UserModel.ts
@@ -0,0 +1,20 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import { IUser } from '../interfaces/user';
+
+export default class UserModel {
+  private connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async create(newUser: IUser): Promise<IUser> {
+    const { username, classe, level, password } = newUser;
+    const [{ insertId }] = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Users (username, classe, level, password ) VALUES (?, ?, ?, ?)',
+      [username, classe, level, password],
+    );
+    // console.log('result', result.insertId);
+    return { id: insertId, ...newUser };
+  }  
+}
