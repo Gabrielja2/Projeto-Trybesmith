e20143683e889b6e1206836c13f8845d58bc544d
Gabriel <gabrielja2antunes@gmail.com>
2022-10-06 22:37:14 -0300

    feat: criando orderRouter

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,7 +1,9 @@
 import productRouter from './productRouter';
 import userRouter from './userRouter';
+import orderRouter from './orderRouter';
 
 export = {
   productRouter,
   userRouter,
+  orderRouter,
 };
