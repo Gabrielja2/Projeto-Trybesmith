2e815c256a8491db1039c96e27c0b84067055ab0
Gabriel <gabrielja2antunes@gmail.com>
2022-10-06 22:36:25 -0300

    feat: adicionando rota orders no APP

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ app.use(express.json());
 
 app.use('/products', route.productRouter);
 app.use('/users', route.userRouter);
+app.use('/orders', route.orderRouter);
 
 app.use(httpErrorMiddleware);
 export default app;
